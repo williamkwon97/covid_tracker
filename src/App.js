a56b5348f802384d350b9f2eff34e11f24b59a3f
William Kwon <williamkwon97@gmail.com>
2020-09-26 19:55:59 -0500

    final version

--- src/App.js
+++ src/App.js
@@ -4,18 +4,42 @@ import React from "react";
 //import Chart from "./components/Chart/Chart";
 //import CountryPicker from "./components/CountryPicker/CountryPicker";
 
-import { Cards, Chart, CountryPicker } from "./components";
+import { Cards, CountryPicker, Chart } from "./components";
+import { fetchData } from "./api/";
 import styles from "./App.module.css";
 
+import image from "./images/image.png";
+
 class App extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  };
+
   render() {
+    const { data, country } = this.state;
+
     return (
       <div className={styles.container}>
-        <Cards />
-        <CountryPicker />
-        <Chart />
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
       </div>
     );
   }
 }
+
 export default App;
--- src/components/api/index.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-const url = 'httpsL..covid19.mathhrdo.id/api';
-const fetchdata = async() => P
-try{
-    const response - await axios.get(url)''
-}
