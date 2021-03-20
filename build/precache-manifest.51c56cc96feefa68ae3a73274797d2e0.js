7b7d490e5dae6add4c3cc9ba023ac3452b2961a5
William Kwon <admin@Williams-Mac-Pro.local>
2021-03-19 23:02:34 -0500

    readme

---
+++ build/precache-manifest.51c56cc96feefa68ae3a73274797d2e0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78d03a30b425da2798733b5dc991711",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a4e37017934c1065ed2",
+    "url": "/static/css/main.1c8917a2.chunk.css"
+  },
+  {
+    "revision": "0e310e78ad23dc1b155c",
+    "url": "/static/js/2.991003f0.chunk.js"
+  },
+  {
+    "revision": "a07000321a1ebac9c07126414b759ee0",
+    "url": "/static/js/2.991003f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a4e37017934c1065ed2",
+    "url": "/static/js/main.3efda2ce.chunk.js"
+  },
+  {
+    "revision": "0f141f1a15ea4bbbb834",
+    "url": "/static/js/runtime-main.ca98175e.js"
+  },
+  {
+    "revision": "08708a2fc6a99202028d4671a9f7755e",
+    "url": "/static/media/image.08708a2f.png"
+  }
+]);
